30a0b0ce82fcc701712240cb0d4750a5dc7cbb2c
Henry Hsu <hhsu@zendesk.com>
2018-09-18 10:30:34 -0700

    Remove secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CallRoulette::Application.config.secret_token = 'a58807408ebdc1d710e7dff63437055860b2410adf3aba4b6e57bda3b3f9a35a185922434840fb6957eba1756c5f91e2461b62d8cd355a24a36751ff581b9da0'
+
+CallRoulette::Application.config.secret_token = ENV.fetch('CR_SECRET_TOKEN') { raise 'Run: export CR_SECRET_TOKEN=`rake secret`'}
